f0a9bff9a5264270b28044202dd942503f13c49b
Scream <scream870102@gmail.com>
2021-01-01 19:00:26 +0900

    大園桃子我婆 應該不會有人發現吧

--- GifMaker/MainWindow.xaml.cs
+++ GifMaker/MainWindow.xaml.cs
@@ -14,7 +14,8 @@ namespace GifMaker
     public partial class MainWindow : Window
     {
         MediaInfo mediaInfo = null;
-        const double sliderTimerTick = 0.3;
+        Rect rect = null;
+        const double sliderTimerTick = 0.001;
         StringBuilder log = new StringBuilder();
         DispatcherTimer timer = null;
         Point rectStart = default(Point);
@@ -22,6 +23,7 @@ namespace GifMaker
         double fpsSec = 0.0;
         bool isPlay = false;
         bool isEditingName = false;
+        bool isMosueOnSlider = false;
 
         public MainWindow()
         {
@@ -37,30 +39,37 @@ namespace GifMaker
 
             //Slider Event Bind
             Slider.MouseLeftButtonUp += Slider_MouseLeftButtonUp;
+            Slider.MouseEnter += Slider_MouseEnter;
+            Slider.MouseLeave += Slider_MouseLeave;
 
             //Media Control Button Bind
             PlayButton.Click += PlayButton_Click;
             KeyDown += MainWindow_KeyDown;
             FromButton.Click += FromButton_Click;
             ToButton.Click += ToButton_Click;
+            SeekButton.Click += SeekButton_Click;
 
             //Rect Event Bind
             Media.MediaOpened += Media_MediaOpened;
             Media.MouseMove += Media_MouseMove;
             Media.MouseLeftButtonDown += Media_MouseLeftButtonDown;
+            Media.MouseRightButtonDown += Media_MouseRightButtonDown;
+            RangeRect.MouseLeftButtonDown += RangeRect_MouseLeftButtonDown;
             RangeRect.MouseRightButtonDown += RangeRect_MouseRightButtonDown;
-
+            MediaRect.MouseLeave += MediaRect_MouseLeave;
+            XTextBox.TextChanged += XTextBox_TextChanged;
+            YTextBox.TextChanged += YTextBox_TextChanged;
+            WidthTextBox.TextChanged += WidthTextBox_TextChanged;
+            HeightTextBox.TextChanged += HeightTextBox_TextChanged;
 
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(sliderTimerTick);
             timer.Tick += Timer_Tick;
             timer.Start();
 
-            UpdateLog("", true);
+            UpdateLog($"", true);
 
         }
-
-
 
         #region UI_BUTTON_CALLBACK
         private void OpenButton_Click(object sender, RoutedEventArgs e)
@@ -75,16 +84,12 @@ namespace GifMaker
 
         private void ClearButton_Click(object sender, RoutedEventArgs e)
         {
-            WidthTextBox.Text = "0";
-            HeightTextBox.Text = "0";
             FromTextBox.Text = "0.0";
             ToTextBox.Text = mediaInfo?.Duration.ToString() ?? "0.0";
-            XTextBox.Text = "";
-            YTextBox.Text = "";
+            rect.Reset();
+            UpdateRect();
             FpsTextBox.Text = mediaInfo?.FrameRate.ToString() ?? "0";
-            RangeRect.Margin = new Thickness(0.0, 0.0, 0.0, 0.0);
-            RangeRect.Width = 0;
-            RangeRect.Height = 0;
+            ScaleTextBox.Text = "-1:-1";
             isMouseDown = false;
             UpdateLog("Clear Setting\n");
         }
@@ -95,26 +100,16 @@ namespace GifMaker
             path.Append($"\\{ExportTextBox.Text}.gif");
             var command = new StringBuilder();
             var dur = Convert.ToDouble(ToTextBox.Text) - Convert.ToDouble(FromTextBox.Text);
-            if (WidthTextBox.Text == "0" || HeightTextBox.Text == "0")
+            if (rect.Width == 0 || rect.Height == 0)
                 command.Append($"-ss {FromTextBox.Text} -t {dur} -i \"{Media.Source.LocalPath}\" -vf \"fps={FpsTextBox.Text},scale = {ScaleTextBox.Text}:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\" -loop 0 \"{path.ToString()}\"");
             else
-                command.Append($"-ss {FromTextBox.Text} -t {dur} -i \"{Media.Source.LocalPath}\" -vf \"fps={FpsTextBox.Text},crop = {WidthTextBox.Text}:{HeightTextBox.Text}:{XTextBox.Text}:{YTextBox.Text},scale = {ScaleTextBox.Text}:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\" -loop 0 \"{path.ToString()}\"");
+                command.Append($"-ss {FromTextBox.Text} -t {dur} -i \"{Media.Source.LocalPath}\" -vf \"fps={FpsTextBox.Text},crop = {rect.Width}:{rect.Height}:{rect.X}:{rect.Y},scale = {ScaleTextBox.Text}:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\" -loop 0 \"{path.ToString()}\"");
             UpdateLog($"Export Command : {command}\n\n");
             FFMpegHandler.ExecuteFFMpeg(command.ToString());
             Process.Start("explorer.exe", "/select, \"" + Assembly.GetEntryAssembly().Location);
         }
 
-        private void ResetButton_Click(object sender, RoutedEventArgs e)
-        {
-            WidthTextBox.Text = "0";
-            HeightTextBox.Text = "0";
-            XTextBox.Text = "";
-            YTextBox.Text = "";
-            RangeRect.Margin = new Thickness(0.0, 0.0, 0.0, 0.0);
-            RangeRect.Width = 0;
-            RangeRect.Height = 0;
-            isMouseDown = false;
-        }
+        private void ResetButton_Click(object sender, RoutedEventArgs e) => ResetRect();
 
         private void ExportTextBox_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e) => isEditingName = false;
 
@@ -122,6 +117,11 @@ namespace GifMaker
         #endregion
 
         private void Slider_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) => Media.Position = TimeSpan.FromSeconds(Slider.Value);
+
+        private void Slider_MouseLeave(object sender, MouseEventArgs e) => isMosueOnSlider = false;
+
+        private void Slider_MouseEnter(object sender, MouseEventArgs e) => isMosueOnSlider = true;
+
 
         #region MEDIA_CONTROL_BUTTON_CALLBACK
         private void PlayButton_Click(object sender, RoutedEventArgs e) => ToggleMediaPlayPause();
@@ -142,15 +142,17 @@ namespace GifMaker
                 Media.Position = TimeSpan.FromSeconds(Slider.Value + fpsSec);
             else if (e.Key == Key.Z)
                 Media.Position = TimeSpan.FromSeconds(Slider.Value - fpsSec);
-            if(e.Key==Key.C)
-                FromTextBox.Text = Media.Position.TotalSeconds.ToString();
-            if(e.Key==Key.V)
-                ToTextBox.Text = Media.Position.TotalSeconds.ToString();
+            if (e.Key == Key.C)
+                FromTextBox.Text = GetCurrentMediaPosition();
+            if (e.Key == Key.V)
+                ToTextBox.Text = GetCurrentMediaPosition();
         }
 
-        private void FromButton_Click(object sender, RoutedEventArgs e) => FromTextBox.Text = Media.Position.TotalSeconds.ToString();
+        private void FromButton_Click(object sender, RoutedEventArgs e) => FromTextBox.Text = GetCurrentMediaPosition();
 
-        private void ToButton_Click(object sender, RoutedEventArgs e) => ToTextBox.Text = Media.Position.TotalSeconds.ToString();
+        private void ToButton_Click(object sender, RoutedEventArgs e) => ToTextBox.Text = GetCurrentMediaPosition();
+
+        private void SeekButton_Click(object sender, RoutedEventArgs e) => Media.Position = TimeSpan.FromSeconds(Convert.ToDouble(SeekTimeTextBox.Text));
         #endregion
 
         #region RECT_CALLBACK
@@ -164,13 +166,13 @@ namespace GifMaker
                 UpdateLog($"Open File Succeess\n");
                 ShellFile shellFile = ShellFile.FromFilePath(Media.Source.LocalPath);
                 mediaInfo = new MediaInfo(Media.NaturalVideoWidth, Media.NaturalVideoHeight, Media.NaturalDuration.TimeSpan.TotalSeconds, (uint)(shellFile.Properties.System.Video.FrameRate.Value / 1000));
-                WidthTextBox.Text = "0";
-                HeightTextBox.Text = "0";
+                rect = new Rect(mediaInfo, Media);
+                UpdateRect();
                 FromTextBox.Text = "0.0";
                 ToTextBox.Text = mediaInfo.Duration.ToString();
+                ScaleTextBox.Text = "-1:-1";
                 FpsTextBox.Text = mediaInfo.FrameRate.ToString();
                 Slider.Maximum = mediaInfo.Duration;
-
                 fpsSec = 1.0 / mediaInfo.FrameRate;
 
                 //Update Media Info
@@ -179,6 +181,7 @@ namespace GifMaker
                 info.Append($"Height : {mediaInfo.Height}\n");
                 info.Append($"Duration : {TimeSpan.FromSeconds(mediaInfo.Duration).ToString(@"mm\:ss")}\n");
                 info.Append($"Fps : {mediaInfo.FrameRate}\n");
+                info.Append($"Frame/sec : {fpsSec.ToString("0.000")} sec\n");
                 FileInfoTextBlock.Text = info.ToString();
             }
         }
@@ -187,10 +190,18 @@ namespace GifMaker
         {
             if (isMouseDown)
             {
-                var currentPos = Mouse.GetPosition(MediaRect);
-                var hw = currentPos - rectStart;
-                RangeRect.Width = Math.Abs(hw.X);
-                RangeRect.Height = Math.Abs(hw.Y);
+                var rectEnd = Mouse.GetPosition(MediaRect);
+                var hw = rectEnd - rectStart;
+                hw.X = Math.Abs(hw.X);
+                hw.Y = Math.Abs(hw.Y);
+                rect.Offset = new Point(hw.X, hw.Y);
+
+                var tmp = default(Point);
+                tmp.X = Math.Min(rectStart.X, rectEnd.X);
+                tmp.Y = Math.Min(rectStart.Y, rectEnd.Y);
+                rect.LeftTopPoint = new Point(tmp.X, tmp.Y);
+                UpdateRect();
+
             }
         }
 
@@ -198,37 +209,88 @@ namespace GifMaker
         {
             if (!isMouseDown)
             {
+                rect.Reset();
                 isMouseDown = true;
                 rectStart = Mouse.GetPosition(MediaRect);
-                RangeRect.Margin = new Thickness(rectStart.X, rectStart.Y, 0.0, 0.0);
-                RangeRect.Width = 0;
-                RangeRect.Height = 0;
-                var mousePoint = Mouse.GetPosition(Media);
-                XTextBox.Text = Convert.ToInt32(mousePoint.X * mediaInfo.Width / Media.ActualWidth).ToString();
-                YTextBox.Text = Convert.ToInt32(mousePoint.Y * mediaInfo.Height / Media.ActualHeight).ToString();
+                rect.LeftTopPoint = rectStart;
+                UpdateRect();
             }
         }
 
-        private void RangeRect_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        private void Media_MouseRightButtonDown(object sender, MouseButtonEventArgs e) => ResetRect();
+
+        private void RangeRect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => isMouseDown = false;
+
+        private void RangeRect_MouseRightButtonDown(object sender, MouseButtonEventArgs e) => ResetRect();
+
+        private void MediaRect_MouseLeave(object sender, MouseEventArgs e) => isMouseDown = false;
+
+        private void XTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (isMouseDown)
+            if (Keyboard.FocusedElement != XTextBox)
+                return;
+            uint res;
+            if (uint.TryParse(XTextBox.Text, out res))
             {
-                WidthTextBox.Text = Convert.ToInt32(RangeRect.ActualWidth * mediaInfo.Width / Media.ActualWidth).ToString();
-                HeightTextBox.Text = Convert.ToInt32(RangeRect.ActualHeight * mediaInfo.Height / Media.ActualHeight).ToString();
-                isMouseDown = false;
+                res = Math.Clamp(res, 0, (uint)mediaInfo.Width-rect.Width);
+                rect.X = res;
+                UpdateRect();
             }
         }
+
+        private void YTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Keyboard.FocusedElement != YTextBox)
+                return;
+            uint res;
+            if (uint.TryParse(YTextBox.Text, out res))
+            {
+                res =Math.Clamp(res, 0, (uint)mediaInfo.Height-rect.Height);
+                rect.Y = res;
+                UpdateRect();
+            }
+        }
+
+        private void WidthTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Keyboard.FocusedElement != WidthTextBox)
+                return;
+            uint res;
+            if (uint.TryParse(WidthTextBox.Text, out res))
+            {
+                res = Math.Clamp(res, 0, (uint)mediaInfo.Width-rect.X);
+                rect.Width = res;
+                UpdateRect();
+            }
+        }
+
+        private void HeightTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Keyboard.FocusedElement != HeightTextBox)
+                return;
+            uint res;
+            if (uint.TryParse(HeightTextBox.Text, out res))
+            {
+                res = Math.Clamp(res, 0, (uint)mediaInfo.Height-rect.Y);
+                rect.Height = res;
+                UpdateRect();
+            }
+        }
+
         #endregion
 
         private void Timer_Tick(object sender, EventArgs e)
         {
             if (Media.Source != null)
             {
-                Slider.Value = Media.Position.TotalSeconds;
+                if (!isMosueOnSlider)
+                    Slider.Value = Media.Position.TotalSeconds;
                 if (Media.NaturalDuration.HasTimeSpan)
-                    TimeTextBox.Text = $"{Media.Position.TotalSeconds.ToString("0.000")} sec \\ {Media.NaturalDuration.TimeSpan.TotalSeconds.ToString("0.000")} sec";
+                    TimeTextBox.Text = $"{GetCurrentMediaPosition()} \\ {Media.NaturalDuration.TimeSpan.TotalSeconds.ToString("0.000")} sec";
             }
         }
+
+        private string GetCurrentMediaPosition() => (Media.Position.TotalSeconds).ToString("0.000");
 
         private void ToggleMediaPlayPause()
         {
@@ -252,7 +314,26 @@ namespace GifMaker
             if (isClear)
                 log.Clear();
             OutputLog.Text = log.Append(context).ToString();
+            LogScrollViewer.ScrollToBottom();
+        }
 
+        private void ResetRect()
+        {
+            rect.Reset();
+            UpdateRect();
+            isMouseDown = false;
+        }
+
+        void UpdateRect()
+        {
+            RangeRect.Margin = new Thickness(rect.LeftTopPoint.X, rect.LeftTopPoint.Y, 0.0, 0.0);
+            RangeRect.Width = rect.Offset.X;
+            RangeRect.Height = rect.Offset.Y;
+
+            XTextBox.Text = rect.X.ToString();
+            YTextBox.Text = rect.Y.ToString();
+            WidthTextBox.Text = rect.Width.ToString();
+            HeightTextBox.Text = rect.Height.ToString();
         }
 
 
@@ -271,6 +352,115 @@ namespace GifMaker
             Height = height;
             Duration = duration;
             FrameRate = frameRate;
+        }
+    }
+
+    class Rect
+    {
+        uint x = 0;
+        uint y = 0;
+        uint width = 0;
+        uint height = 0;
+        Point leftTopPoint = default(Point);
+        Point offset = default(Point);
+        public uint X
+        {
+            get => x;
+            set
+            {
+                x = value;
+                UpdateLeftTopPoint();
+            }
+        }
+        public uint Y
+        {
+            get => y; 
+            set
+            {
+                y = value;
+                UpdateLeftTopPoint();
+            }
+        }
+        public uint Width
+        {
+            get => width; 
+            set
+            {
+                width = value;
+                UpdateOffset();
+            }
+        }
+        public uint Height
+        {
+            get => height; 
+            set
+            {
+                height = value;
+                UpdateOffset();
+            }
+        }
+        public Point LeftTopPoint
+        {
+            get => leftTopPoint; 
+            set
+            {
+                leftTopPoint = value;
+                UpdateXY();
+            }
+        }
+        public Point Offset
+        {
+            get => offset; 
+            set
+            {
+                offset = value;
+                UpdateWH();
+            }
+        }
+
+        MediaInfo mediaInfo = null;
+        MediaElement mediaElement = null;
+
+        public Rect(MediaInfo mediaInfo, MediaElement mediaElement)
+        {
+            this.mediaInfo = mediaInfo;
+            this.mediaElement = mediaElement;
+        }
+
+        public void Reset()
+        {
+            x = 0;
+            y = 0;
+            width = 0;
+            height = 0;
+            leftTopPoint = default(Point);
+            offset = default(Point);
+        }
+
+        void UpdateXY()
+        {
+            x = Convert.ToUInt32(LeftTopPoint.X * mediaInfo.Width / mediaElement.ActualWidth);
+            y = Convert.ToUInt32(LeftTopPoint.Y * mediaInfo.Height / mediaElement.ActualHeight);
+        }
+
+        void UpdateWH()
+        {
+            width = Convert.ToUInt32(Offset.X * mediaInfo.Width / mediaElement.ActualWidth);
+            height = Convert.ToUInt32(Offset.Y * mediaInfo.Height / mediaElement.ActualHeight);
+        }
+
+        void UpdateLeftTopPoint()
+        {
+            var lx = X * mediaElement.ActualWidth / mediaInfo.Width;
+            var ly = Y * mediaElement.ActualHeight / mediaInfo.Height;
+            leftTopPoint = new Point(lx, ly);
+        }
+
+        void UpdateOffset()
+        {
+            var ox = Width * mediaElement.ActualWidth / mediaInfo.Width;
+            var oy = Height * mediaElement.ActualHeight / mediaInfo.Height;
+            offset = new Point(ox, oy);
         }
     }
 }
---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+Keyboard Shortcuts
+[ESC] Leave TextBox Focus
+[Z] Previous Frame
+[X] Next Frame
+[C] Set Current Position as From Point
+[V] Set Current Postion as To Point
+[Space] Toggle between Play and Pause
+
